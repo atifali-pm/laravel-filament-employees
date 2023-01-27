a8f65cb226f7e72f542aa34bb88acd0048786a8e
Atif <atif@monotote.com>
2023-01-27 19:37:56 +0500

    Update: employees resource added to department, country and cities

--- app/Filament/Resources/CountryResource/RelationManagers/EmployeesRelationManager.php
+++ app/Filament/Resources/CountryResource/RelationManagers/EmployeesRelationManager.php
@@ -2,11 +2,17 @@
 
 namespace App\Filament\Resources\CountryResource\RelationManagers;
 
+use App\Models\Country;
+use App\Models\State;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
@@ -20,9 +26,39 @@ class EmployeesRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('firstname')
+                TextInput::make('firstname')->required()->maxLength(255),
+                TextInput::make('lastname')->required()->maxLength(255),
+                TextInput::make('address')->required()->maxLength(255),
+                TextInput::make('zip_code')->required()->maxLength(5),
+                DatePicker::make('birth_date')->required(),
+                DatePicker::make('date_hired')->required(),
+                Select::make('department_id')->relationship('department', 'name')->required(),
+                Select::make('country_id')
+                    ->label('Country')
+                    ->reactive()
+                    ->options(Country::all()->pluck('name', 'id')->toArray())
+                    ->reactive(),
+                Select::make('state_id')
+                    ->label('State')
                     ->required()
-                    ->maxLength(255),
+                    ->options(function (callable $get) {
+                        $country = Country::find($get('country_id'));
+                        if (!$country) {
+                            return State::all()->pluck('name', 'id');
+                        }
+                        return $country->states->pluck('name', 'id');
+                    })
+                    ->reactive(),
+                Select::make('city_id')
+                    ->label('City')
+                    ->required()
+                    ->options(function (callable $get) {
+                        $state = State::find($get('state_id'));
+                        if (!$state) {
+                            return State::all()->pluck('name', 'id');
+                        }
+                        return $state->cities->pluck('name', 'id');
+                    }),
             ]);
     }
 
@@ -30,7 +66,12 @@ class EmployeesRelationManager extends RelationManager
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('firstname'),
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('firstname')->sortable()->searchable(),
+                TextColumn::make('lastname')->sortable()->searchable(),
+                TextColumn::make('department.name')->sortable()->searchable(),
+                TextColumn::make('date_hired')->date()->sortable()->searchable(),
+                TextColumn::make('created_at')->dateTime()->sortable(),
             ])
             ->filters([
                 //
@@ -45,5 +86,5 @@ class EmployeesRelationManager extends RelationManager
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
-    }    
+    }
 }
--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -11,12 +13,12 @@ class City extends Model
 
     protected $fillable = ['state_id', 'name'];
 
-    public function state()
+    public function state(): BelongsTo
     {
         return $this->belongsTo(State::class);
     }
 
-    public function employees()
+    public function employees(): HasMany
     {
         return $this->hasMany(Employee::class);
     }
